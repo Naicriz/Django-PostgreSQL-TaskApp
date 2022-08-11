72c3f822a849f329633043f6ab4cb372803c6628
isalazarjara@outlook.com <Ian Salazar>
2022-08-10 23:38:42 -0400

    ToDo app finished some time ago.

---
+++ tasks/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import delete_task, list_tasks, create_task
+
+urlpatterns = [
+    path('', list_tasks),
+    path('new/', create_task, name='create_task'),
+    path('<int:task_id>/delete/', delete_task, name='delete_task'),
+]
